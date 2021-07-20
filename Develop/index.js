636f375f444b38949ea4e09cf531d3edd534acf4
darrenthamtoro <darrenthamtoro@gmail.com>
2021-07-20 16:51:19 -0700

    inital index.js

---
+++ Develop/index.js
@@ -0,0 +1,12 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+
+function init(){
+    console.log("Building Team Profile......."); 
+
+}
+
+init(); 
